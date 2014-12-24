a3b6bd50d3add086e42709d695bee4f25ab3fb5b
chiaS <pretend15@gmail.com>
2014-12-24 11:02:33 -0800

    set Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ var app = express();
 app.use(bodyParser.json());
 
 require('./config/globals');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/api', router);
 //app.use(express.static(path.join(__dirname, '/../client')));
 // app.get('/', function (req, res) {
